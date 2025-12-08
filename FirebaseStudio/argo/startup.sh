e8d40cd0e36f5e23de6e5355d89a27c67231c771
justlagom <93416015+justlagom@users.noreply.github.com>
2025-12-08 14:18:13 +0800

    Update startup.sh

--- FirebaseStudio/argo/startup.sh
+++ FirebaseStudio/argo/startup.sh
@@ -3,7 +3,7 @@
 ARGO_TOKEN=
 
 if [ -z "$ARGO_TOKEN" ]; then
-  nohup $PWD/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 --url http://localhost:8090 1>$PWD/argo.log 2>&1 &
+  nohup $PWD/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 --url http://localhost:8001 1>$PWD/argo.log 2>&1 &
 else
   nohup $PWD/cloudflared tunnel --no-autoupdate --edge-ip-version auto --protocol http2 run --token $ARGO_TOKEN 1>$PWD/argo.log 2>&1 &
 fi
